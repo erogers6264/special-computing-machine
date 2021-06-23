533405e073580de67f0b9ca4e7a01b7c92aed491
Ethan Rogers <erogers6264@gmail.com>
2021-06-23 16:25:48 -0600

    Define some colors, score, lives

--- breakout.py
+++ breakout.py
@@ -1,3 +1,14 @@
 # Import the pygame library and intialize the game engine.
 import pygame
 pygame.init()
+
+# Define some colors
+GREEN = (255, 255, 255)
+PURPLE = (255, 255, 255)
+DARKBLUE = (255, 255, 255)
+YELLOW = (255, 255, 255)
+RED = (255, 255, 255)
+ORANGE = (255, 255, 255)
+
+score = 0
+lives = 3
