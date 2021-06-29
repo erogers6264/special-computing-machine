b893d841112b6003b8a43a7fef85d44e838c9191
Ethan Rogers <erogers6264@gmail.com>
2021-06-29 14:41:10 -0600

    Fill the screen with dark blue

--- breakout.py
+++ breakout.py
@@ -3,9 +3,9 @@ import pygame
 pygame.init()
 
 # Define some colors
-GREEN = (255, 255, 255)
+GREEN = (0, 255, 0)
 PURPLE = (255, 255, 255)
-DARKBLUE = (255, 255, 255)
+DARKBLUE = (16, 23, 99)
 YELLOW = (255, 255, 255)
 RED = (255, 255, 255)
 ORANGE = (255, 255, 255)
@@ -17,3 +17,20 @@ lives = 3
 size = (800, 600)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Breakout Game")
+
+# The main program loop
+carryOn = True
+
+clock = pygame.time.Clock()
+
+while carryOn:
+    # Capturing Events
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT:
+            carryOn = False
+    # Implementing Game Logic
+
+    # Refreshing Screen
+    screen.fill(DARKBLUE)
+    
+    pygame.display.flip()
