19551f11c70ccf3c792a3b1d07d5ea2eb0b42092
Ethan Rogers <erogers6264@gmail.com>
2021-06-30 10:50:26 -0600

    Prevent a 0 y velocity

--- ball.py
+++ ball.py
@@ -13,7 +13,7 @@ class Ball(pygame.sprite.Sprite):
         
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         
-        self.velocity = [randint(4, 8), randint(-8, 8)]
+        self.velocity = [randint(4, 8), randint(-9, 7) + 1]
         
         self.rect = self.image.get_rect()
         
